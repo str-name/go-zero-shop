6602f2db5a11708ede5e4675525f2a3cdee59cf3
str-name <67568397+str-name@users.noreply.github.com>
2023-09-11 15:34:06 +0800

    Update payment.go

--- common/gorm/payment.go
+++ common/gorm/payment.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-const PaymentDSN = "root:gaj991130@tcp(127.0.0.1:3306)/zero_shop_payment?charset=utf8mb4&parseTime=True&loc=Local"
+const PaymentDSN = "root:123456@tcp(127.0.0.1:3306)/zero_shop_payment?charset=utf8mb4&parseTime=True&loc=Local"
 
 var PaymentDB *gorm.DB
 
