f04254bcd9d283ab336264532d5bcaa9244601a7
str-name <67568397+str-name@users.noreply.github.com>
2023-09-11 15:34:31 +0800

    Update user.go

--- common/gorm/user.go
+++ common/gorm/user.go
@@ -11,7 +11,7 @@ import (
 	"time"
 )
 
-const UserDSN = "root:gaj991130@tcp(127.0.0.1:3306)/zero_shop_user?charset=utf8mb4&parseTime=True&loc=Local"
+const UserDSN = "root:123456@tcp(127.0.0.1:3306)/zero_shop_user?charset=utf8mb4&parseTime=True&loc=Local"
 
 var UserDB *gorm.DB
 
